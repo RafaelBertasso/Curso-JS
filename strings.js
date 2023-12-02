495748dd57212854a9e5f1c3c444ca08cbbef803
Bowie <Rafaelbertasso06@gmail.com>
2023-12-02 14:38:22 -0300

    criei dois exs

---
+++ strings.js
@@ -0,0 +1,9 @@
+const texto1 = "Olá, mundo"
+const texto2 = 'Olá, mundo'
+const senha = 'abc123'
+const num = "32145"
+
+//concatenação
+const citacao = 'Meu nome é: '
+const nome = "Rafael"
+console.log(citacao + nome)
---
+++ tiposDados.js
@@ -0,0 +1,23 @@
+//tipo Number
+
+const meuNumero = 3
+
+const num1 = 1
+const num2 = 2
+
+const op = num1 * num2
+
+console.log(op)
+
+// float
+
+const float = 3.3
+const float2 = .5 //0.5
+
+const op2 = num2 / float2
+console.log(op2)
+
+//NaN -> Not a Number
+
+const alura = "Alura"
+console.log(alura * num1)
