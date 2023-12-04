b23a8f0c7468a90eb77d9d98f803b370889dd213
Bowie <Rafaelbertasso06@gmail.com>
2023-12-04 20:48:19 -0300

    criei ex

---
+++ variaveis.js
@@ -0,0 +1,42 @@
+//var
+
+// var altura = 5
+// var comprimento = 7
+
+// area = altura * comprimento
+
+// console.log(area)
+
+// var area
+
+//let
+
+// let altura = 5
+// let comprimento = 7
+// let forma = 'retangulo'
+// let area
+
+// if (forma === 'retangulo') {
+//     area = altura * comprimento
+// }
+// else {
+//     area = (altura * comprimento) / 2
+// }
+
+// console.log(area)
+
+// const
+
+const forma = 'triangulo'
+const altura = 5
+const comprimento = 7
+let area = altura * comprimento
+
+if (forma === 'quadrado') {
+    area
+}
+else {
+    area = (altura * comprimento) / 2
+}
+
+console.log(area)
