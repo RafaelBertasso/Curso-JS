ebc3fbd789dc648bbb7a45cfb09025fef0047fa8
Bowie <Rafaelbertasso06@gmail.com>
2023-12-07 17:47:57 -0300

    exs

---
+++ comparacao.js
@@ -0,0 +1,8 @@
+// == -> comparação implicita (valor)
+// === -> comparação explicita (valor e tipo)
+let numero = 10
+let texto = '10'
+console.log(numero === texto)
+
+console.log(typeof numero)
+console.log(typeof texto)
---
+++ optern.js
@@ -0,0 +1,12 @@
+let idMin = 18
+let idCli = 16
+
+// if(idCli >= idMin) {
+//     console.log("Coca")
+// }
+// else {
+//     console.log("Suco")
+// }
+
+//console.log(condicao ? saida true : saida false)
+console.log(idCli >= idMin ? "Coca" : "Suco")
