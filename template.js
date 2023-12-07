596e500de998cc513d8e951d631eff0df9a28f26
Bowie <Rafaelbertasso06@gmail.com>
2023-12-07 18:02:10 -0300

    ex

---
+++ template.js
@@ -0,0 +1,11 @@
+// template string (`${nomeVar}`)
+
+let nome = "Rafael"
+let idade = 18
+let cidade = "SJRP"
+
+// apre = f"texto{var}"
+let apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci em ${cidade}`
+
+
+console.log(apresentacao)
