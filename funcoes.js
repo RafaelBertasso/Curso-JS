6fe238ba0f14af5f239e7c1346c0ab1470731d68
Bowie <Rafaelbertasso06@gmail.com>
2024-01-08 09:55:02 -0300

    aula funcao

---
+++ funcoes.js
@@ -0,0 +1,15 @@
+// DECLARAÇÃO DE FUNÇÃO
+
+// declarar a função
+function imprimeTexto(texto) {
+    console.log(texto)
+}
+
+// executar a função
+imprimeTexto(soma(10,2))
+imprimeTexto('oi')
+
+function soma(a,b){
+    return a+b
+}
+//console.log(soma(10,2))
